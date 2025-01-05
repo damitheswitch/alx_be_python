e931a0ef23df40a661ee9d671e7d99b59c701a1f
damitheswitch <charradiimad7@gmail.com>
2025-01-05 16:42:17 +0800

    Add basic arithmetic operations exercise

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 4
+number2 = 7
+
+add = number1 + number2
+sub = number1 - number2
+multp = number1 * number2
+
+print(f"Addition of {number1} and {number2} is {add}")
+print(f"Substraction of {number1} and {number2} is {sub}")
+print(f"Multiplication of {number1} and {number2} is {multp}")
