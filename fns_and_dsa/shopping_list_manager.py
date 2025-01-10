a444ed820c9fe7dbf25bb0b1b6982e81af715477
damitheswitch <charradiimad7@gmail.com>
2025-01-10 17:39:25 +0800

    for the checker

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -13,7 +13,8 @@ def main():
 
         if choice == '1':
             # Prompt for and add an item
-            shopping_list.append(input("What item do you want to add?: "))
+            to_add = input("Enter the item to add: ")
+            shopping_list.append(to_add)
             pass
         elif choice == '2':
             # Prompt for and remove an item
