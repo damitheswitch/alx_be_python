523260717339c9131b7a16453fc2892d5e1437b1
damitheswitch <charradiimad7@gmail.com>
2025-01-17 13:02:13 +0800

    yeah sure

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,40 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self.__is_checkedout = False
+
+    def check_out(self):
+        self.__is_checkedout = True
+
+    def return_book(self):
+        self.__is_checkedout = False
+
+    def is_available(self):
+        return not self.__is_checkedout
+
+        
+class Library:
+    def __init__(self):
+        self.__books = []
+    
+
+    def add_book(self, book):
+        self.__books.append(book)
+
+    def check_out_book(self, title):
+        for book in self.__books:
+            if book.title == title:
+                book.check_out()
+                break
+
+    def return_book(self, title):
+        for book in self.__books:
+            if book.title == title:
+                book.return_book()
+                break
+
+    def list_available_books(self):
+        for book in self.__books:
+            if book.is_available():
+                print(f"{book.title} by {book.author}")
