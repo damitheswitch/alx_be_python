63279d56652905412b8b061a7e90d8468f0bf7bc
damitheswitch <charradiimad7@gmail.com>
2025-03-16 20:55:26 +0800

    rich in oop concepts

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,32 +1,40 @@
 class Book:
-    def __init__(self, title, author):
+    def __init__(self, title: str, author: str):
         self.title = title
         self.author = author
 
+    def __repr__(self):
+        return f"Book: {self.title} by {self.author}"
+        
+
+
 class EBook(Book):
-    def __init__(self, title, author, file_size):
+    def __init__(self, title, author, file_size: int):
         super().__init__(title, author)
         self.file_size = file_size
 
+    def __repr__(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
+
+
 class PrintBook(Book):
-    def __init__(self, title, author, page_count):
+    def __init__(self, title, author, page_count: int):
         super().__init__(title, author)
         self.page_count = page_count
+
+    def __repr__(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+
 
 class Library:
     def __init__(self):
         self.books = []
 
-    def add_book(self, book):
-        self.books.append(book)
+
+    def add_book(self, books):
+        self.books.append(books)
+
 
     def list_books(self):
         for book in self.books:
-            if isinstance(book, EBook):
-                print(f"Ebook: {book.title} by {book.author}, File size:{book.file_size}KB")
-
-            elif isinstance(book, PrintBook):
-                print(f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}")
-
-            else:
-                print(f"Book: {book.title} by {book.author}")
+            print(book)
