206de4f1dd787e10baf2c7a4bedd64a52c12395d
damitheswitch <charradiimad7@gmail.com>
2025-03-17 16:07:37 +0800

    after pc crash commit

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -5,9 +5,8 @@ class Calculator:
     @staticmethod
     def add(a, b):
         return a + b
-
+    
     @classmethod
     def multiply(cls, a, b):
-        print(f"Calculation_type: {cls.calculation_type}")
+        print(f"Calculation type: {cls.calculation_type}")
         return a * b
-
